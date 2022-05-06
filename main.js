022cfeb644976ad0b9aa8994f1b3fb538e38a3cf
sitaaraa <lnicola@gmail.com>
2022-05-06 10:44:29 +0100

    Create Activity History

--- main.js
+++ main.js
@@ -5,44 +5,43 @@ let data = await response.json();
 console.log(data);
 let p = document.querySelector('#activity-response')
 p.innerText = data.activity;
-
-//addToList(h1.innerText);
+addToList(p.innerText);
 }
 
-getActivity(); 
+
 
 // Ties getting new activity to button
 let mainButton = document.querySelector('#main-button');
 mainButton.addEventListener('click', getActivity);
 
 
-function addToList(quote) {
-let list = document.querySelector('#kanye-quote-history');
+function addToList(activity) {
+let list = document.querySelector('#previousIdeas');
 let newListItem = document.createElement('li');
-newListItem.innerText = quote;
+newListItem.innerText = activity;
 list.prepend(newListItem);
-console.log(list.innerHTML);
+console.log(newListItem.innerHTML);
 }
 
 
-let repeatedQuote = document.querySelectorAll('#kanye-quote-history').innerText;
+// let repeatedQuote = document.querySelectorAll('#kanye-quote-history').innerText;
 
-if (h1.innerText !== repeatedQuote) {
-    addToList(h1.innerText);
-} 
+// if (h1.innerText !== repeatedQuote) {
+//  addToList(h1.innerText);
+// } 
 
 
-let launchCode = {"enter": "ROVER"}
-;
-         async function enterCode() {
-            let response = await fetch("https://task-escape-api.herokuapp.com/api/codes", {
-                method: "POST",											
-                headers: { "Content-Type" : "application/json" },		
-                body: JSON.stringify(launchCode)
-            });
-            let result = await response.json();
-            console.log(result);
-         };
+// let launchCode = {"enter": "ROVER"}
+// ;
+//          async function enterCode() {
+//             let response = await fetch("https://task-escape-api.herokuapp.com/api/codes", {
+//                 method: "POST",											
+//                 headers: { "Content-Type" : "application/json" },		
+//                 body: JSON.stringify(launchCode)
+//             });
+//             let result = await response.json();
+//             console.log(result);
+//          };
          
-         enterCode();
+//          enterCode();
 
