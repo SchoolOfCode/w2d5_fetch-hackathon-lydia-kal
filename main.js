cbbc4aab80d806036f4c42984bdc11cf6587208f
kalhollywood <kal.hollywood@gmail.com>
2022-05-06 15:53:42 +0100

    Speed coded an adventure

--- main.js
+++ main.js
@@ -22,6 +22,8 @@ function addToList(activity) {
   console.log(newListItem.innerHTML);
 }
 
+// Get a short story from the shortstories API
+
 async function getStory() {
   let response = await fetch("https://shortstories-api.herokuapp.com/");
   let data = await response.json();
@@ -50,39 +52,44 @@ function storyBorder() {
   let T = document.getElementById("story-text");
   T.style.display = "block";
 }
+function adventureBorder() {
+  let T = document.getElementById("adventureDiv");
+  T.style.display = "block";
+}
 
-var x = document.getElementById("myBtn")
-      x.addEventListener("click", myName);
-      x.addEventListener("click", myAge);
+var x = document.getElementById("myBtn");
+x.addEventListener("click", myName);
+x.addEventListener("click", myAge);
 
-      
-      function myName() {
-        let txt;
-        let person = prompt("Please enter your name:", "Harry Potter");
-        if (person == null || person == "") {
-          txt = "User cancelled the prompt.";
-        } else {
-          txt = "Hello " + person + ", I hope you're having a good day!";
-        }
-        document.getElementById("p1").innerHTML = txt
-      }
+function myName() {
+  let txt;
+  let person = prompt("Please enter your name:", "Harry Potter");
+  if (person == null || person == "") {
+    txt = "User cancelled the prompt.";
+  } else {
+    txt = "Hello " + person + ", I hope you're having a good day!";
+  }
+  document.getElementById("p1").innerHTML = txt;
+}
 
-
-      function myAge() {
-        let txt;
-        let age = prompt("Please enter your age:", 12);
-        if (age == null || age == "") {
-          txt = "User cancelled the prompt."; // if null cancel prompt
-        } else if (age == 11) {
-          txt = "Welcome to Hogwarts! Professor McGonagall will be along to collect you shortly."; //return for 11 Hogwarts
-        } else if (age >= 50) {
-          txt = "You're going on an adventure! Say hi to Smaug :) "; // return for age 50+
-        } else if (age > 11 && age <= 15) {
-          txt = "Go through your wardrobe, you may just find Narnia!"; // Narnia parameters 11 - 15
-        } else if (age < 11 && age >= 0) {
-          txt = "You might be a bit young for magic yet, but stay vigilant! You never know when magical nannies will appear."; // for small children
-        } else {
-          txt = "You may have missed out on a magical adventure so far, but keep checking your mailbox - Vetinari is always on the look out for useful people";
-        }
-               document.getElementById("p2").innerHTML = txt
+function myAge() {
+  let txt;
+  let age = prompt("Please enter your age:", 12);
+  if (age == null || age == "") {
+    txt = "User cancelled the prompt."; // if null cancel prompt
+  } else if (age == 11) {
+    txt =
+      "Welcome to Hogwarts! Professor McGonagall will be along to collect you shortly."; //return for 11 Hogwarts
+  } else if (age >= 50) {
+    txt = "You're going on an adventure! Say hi to Smaug :) "; // return for age 50+
+  } else if (age > 11 && age <= 15) {
+    txt = "Go through your wardrobe, you may just find Narnia!"; // Narnia parameters 11 - 15
+  } else if (age < 11 && age >= 0) {
+    txt =
+      "You might be a bit young for magic yet, but stay vigilant! You never know when magical nannies will appear."; // for small children
+  } else {
+    txt =
+      "You may have missed out on a magical adventure so far, but keep checking your mailbox - Vetinari is always on the look out for useful people";
+  }
+  document.getElementById("p2").innerHTML = txt;
 }
