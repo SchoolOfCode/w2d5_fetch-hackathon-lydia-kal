9f91d77aa0f601bb3eda26c877054b51e3a7f75a
sitaaraa <lnicola@gmail.com>
2022-05-06 11:30:58 +0100

    added user input to API

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 // Pulls data from Get BoredAPI
 async function getActivity() {
-let response = await fetch("http://www.boredapi.com/api/activity?participants=");
+let numberInput = document.querySelector("#numberField").value;   
+const url = `http://www.boredapi.com/api/activity?participants=${numberInput}`
+let response = await fetch(url);
 let data = await response.json();
 console.log(data);
 let p = document.querySelector('#activity-response')
 p.innerText = data.activity;
 addToList(p.innerText);
 }
-
-
 
 // Ties getting new activity to button
 let mainButton = document.querySelector('#main-button');
@@ -19,7 +19,7 @@ function addToList(activity) {
 let list = document.querySelector('#previousIdeas');
 let newListItem = document.createElement('li');
 newListItem.innerText = activity;
-list.prepend(newListItem);
+list.append(newListItem);
 console.log(newListItem.innerHTML);
 }
 
@@ -28,19 +28,4 @@ console.log(newListItem.innerHTML);
 // if (h1.innerText !== repeatedQuote) {
 //  addToList(h1.innerText);
 // } 
-
-
-// let launchCode = {"enter": "ROVER"}
-// ;
-//          async function enterCode() {
-//             let response = await fetch("https://task-escape-api.herokuapp.com/api/codes", {
-//                 method: "POST",											
-//                 headers: { "Content-Type" : "application/json" },		
-//                 body: JSON.stringify(launchCode)
-//             });
-//             let result = await response.json();
-//             console.log(result);
-//          };
-         
-//          enterCode();
 
